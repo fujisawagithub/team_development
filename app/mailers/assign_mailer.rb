fc358ce744597dc29016ba42a2b98c781fd736da
yuki fujisawa <fujisawa.yuki@outlook.com>
2022-01-05 11:42:14 +0900

    Add アジェンダ削除時にTeam全員にメール送信する機能

--- app/mailers/assign_mailer.rb
+++ app/mailers/assign_mailer.rb
@@ -11,4 +11,9 @@ class AssignMailer < ApplicationMailer
     @email = email
     mail to: @email, subject: I18n.t('views.messages.complete_change')
   end
+
+  def deleate_agenda_mail(team_mambers)
+    @team_members = team_mambers
+    mail to: @team_members.map(&:email), subject: I18n.t('views.messages.delete_agenda')
+  end
 end
